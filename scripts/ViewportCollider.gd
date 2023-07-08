4c4794b4463a6a116ef2f189a06cff8a25e8f28a
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-08 21:15:34 +0100

    Spinning

--- scripts/ViewportCollider.gd
+++ scripts/ViewportCollider.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 func _ready():
-	
-	viewport.size = get_viewport_rect().size
-	$CollisionShape2D.set_shape(viewport)
+	print("hello")
+	# viewport.size = get_viewport_rect().size
+	# $CollisionShape2D.set_shape(viewport)
