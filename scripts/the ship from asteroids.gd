08a70073d48aeb37c1a0a60d5378e7e1b0c7904c
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-08 22:31:38 +0100

    Simplified ship

--- scripts/the ship from asteroids.gd
+++ scripts/the ship from asteroids.gd
@@ -2,32 +2,22 @@ extends RigidBody2D
 
 signal enemy_hit
 
-signal collision(id, new_velocity)
-
 @export var accel = 20
 
 @export var speedlimit = 300
 
-const wide_spin = 300
-
-const little_spin = PI
-
 func _process(delta): # _physics_process
-	# Speen
-	rotation += little_spin * delta
-
-	var v = Vector2.UP.rotated(rotation) * wide_spin
-
-	position += v * delta
-
+	pass
 	# velocity.x = move_toward(velocity.x, 0, speed)
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free() # Replace with function body.
+	queue_free()
 
 func _on_body_entered(_body):
 	hide()
 	enemy_hit.emit() # so we can do whatever we want knowing we've hit them
 	$CollisionPolygon2D.set_deferred("disabled", true)
+	queue_free()
 	
 	pass # create an explosion effect?
+	
