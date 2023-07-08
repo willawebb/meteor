97b0e8393e8a6320776412b55df5cfb6d7f6c4fa
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-08 22:46:37 +0100

    Ships disappear on contact

--- scripts/the ship from asteroids.gd
+++ scripts/the ship from asteroids.gd
@@ -14,7 +14,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_body_entered(_body):
-	print("collision")
 	hide()
 	enemy_hit.emit() # so we can do whatever we want knowing we've hit them
 	$CollisionPolygon2D.set_deferred("disabled", true)
