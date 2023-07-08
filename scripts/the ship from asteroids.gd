f35077cc73243e6bdee60389699f97a21884b467
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-08 22:00:38 +0100

    Disappears when offscreen

--- scripts/the ship from asteroids.gd
+++ scripts/the ship from asteroids.gd
@@ -8,8 +8,11 @@ func _process(delta): # _physics_process
 	# Speen
 	rotation += angular_speed * delta
 
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	var v = Vector2.UP.rotated(rotation) * speed
 
-	position += velocity * delta
+	position += v * delta
 
 	# velocity.x = move_toward(velocity.x, 0, speed)
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free() # Replace with function body.
