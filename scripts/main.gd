6c7da939b5c9ec3cacf0c30e4e782d03bc938c0b
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-09 18:26:15 +0100

    No player spawning

--- scripts/main.gd
+++ scripts/main.gd
@@ -52,31 +52,31 @@ func freeze_frame(duration, timescale):
 	Engine.time_scale = 1.0
 
 func _process(delta):
-	
-	if Input.is_action_just_pressed("button_2"):
-		var asteroid = asteroid_scene.instantiate()
-		
-		asteroid.position = get_viewport().get_mouse_position()
-		
-		var direction = randf_range(-PI / 4, PI / 4)
-		
-		var velocity = Vector2(randf_range(150.0, 600.0), 0.0)
-		
-		asteroid.linear_velocity = velocity.rotated(direction)
-		
-		add_child(asteroid)
-		
-	if Input.is_action_just_pressed("button_1"):
-		var ship = ship_scene.instantiate()
-		
-		ship.position = get_viewport().get_mouse_position()
-		
-		var direction = randf_range(-PI / 4, PI / 4)
-		
-		ship.connect("enemy_hit", _on_the_ship_from_asteroids_enemy_hit)
-		
-		add_child(ship)
-		
+#
+#	if Input.is_action_just_pressed("button_2"):
+#		var asteroid = asteroid_scene.instantiate()
+#
+#		asteroid.position = get_viewport().get_mouse_position()
+#
+#		var direction = randf_range(-PI / 4, PI / 4)
+#
+#		var velocity = Vector2(randf_range(150.0, 600.0), 0.0)
+#
+#		asteroid.linear_velocity = velocity.rotated(direction)
+#
+#		add_child(asteroid)
+#
+#	if Input.is_action_just_pressed("button_1"):
+#		var ship = ship_scene.instantiate()
+#
+#		ship.position = get_viewport().get_mouse_position()
+#
+#		var direction = randf_range(-PI / 4, PI / 4)
+#
+#		ship.connect("enemy_hit", _on_the_ship_from_asteroids_enemy_hit)
+#
+#		add_child(ship)
+#
 	if Input.is_action_just_pressed("select"):
 		new_game()
 		
@@ -163,7 +163,5 @@ func shuffle_rails(n: int = 3):
 	for i in range(min(node_count, n)):
 		rail_nodes[randi() % node_count].start()
 
-
 func _on_enemy_rail_rail_spawned(thing):
-	print("This works.")
 	thing.connect("enemy_hit", _on_the_ship_from_asteroids_enemy_hit)
