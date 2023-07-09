bfc762c7ab1a3ade867338b97a79fc066eb1720d
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-07-09 03:58:46 +0100

    Curves

--- scripts/the ship from asteroids.gd
+++ scripts/the ship from asteroids.gd
@@ -9,8 +9,8 @@ var track_progress: float = 0.0
 
 # To change the Gun, alter it in the node inspector of your ship scene
 
-func _physics_process(delta):
-	follow_track(delta)
+func _integrate_forces(state):
+	follow_track(state)
 	# velocity.x = move_toward(velocity.x, 0, speed)
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
@@ -27,11 +27,12 @@ func _on_body_entered(_body):
 func set_track(track: PathFollow2D):
 	track_to_follow = track
 
-func follow_track(delta):
+func follow_track(state):
 	if track_to_follow is PathFollow2D:
-		track_progress += speed * delta
+		track_progress += speed * state.step
 		track_to_follow.progress = track_progress
-		position = track_to_follow.position
+		state.transform.origin.x = track_to_follow.position.x
+		state.transform.origin.y = track_to_follow.position.y
 		rotation = track_to_follow.rotation
 		if track_to_follow.progress_ratio >= 0.98:
 			queue_free()
